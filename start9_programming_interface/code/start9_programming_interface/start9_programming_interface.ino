e2e4e4548c58229d114d2f546934c7c35a16120c
Federico Scozzafava <scozzafava@di.uniroma1.it>
2020-04-23 02:03:39 +0200

    start9

---
+++ start9_programming_interface/code/start9_programming_interface/start9_programming_interface.ino
@@ -0,0 +1,138 @@
+#define MI 
+#define HL 9
+#define RI 12
+#define EN 13
+#define CLK A0
+#define RST A1
+#define CU_EN A2
+
+#define NEXT A4
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  //Serial.setTimeout(5000);
+  pinMode(MI, OUTPUT);
+  pinMode(HL, OUTPUT);
+  pinMode(RI, OUTPUT);
+  pinMode(EN, OUTPUT);
+  pinMode(CLK, INPUT);
+  pinMode(RST, INPUT);
+  pinMode(CU_EN, OUTPUT);
+
+  DDRD |= B11111111;
+}
+
+void reset()
+{
+  pinMode(RST, OUTPUT);
+  digitalWrite(RST, HIGH);
+  delay(10);
+  digitalWrite(RST, LOW);
+  pinMode(RST, INPUT);
+}
+
+void clock()
+{
+  pinMode(CLK, OUTPUT);
+  digitalWrite(CLK, HIGH);
+  delay(1);
+  digitalWrite(CLK, LOW);
+  pinMode(CLK, INPUT);
+}
+
+void enableClock()
+{
+  pinMode(CLK, OUTPUT);
+  digitalWrite(CLK, HIGH);
+}
+
+void disableClock()
+{
+  pinMode(CLK, OUTPUT);
+  digitalWrite(CLK, LOW);
+  pinMode(CLK, INPUT);
+}
+
+void disable_cu()
+{
+  digitalWrite(CU_EN, HIGH);
+}
+
+void enable_cu()
+{
+  digitalWrite(CU_EN, LOW);
+}
+
+void enableOutput()
+{
+  digitalWrite(EN, HIGH);
+}
+
+void disableOutput()
+{
+  digitalWrite(EN, LOW);
+}
+
+void setAddress(unsigned int address)
+{
+  digitalWrite(HL, address > 0xFF);
+  putOut(address);
+  delay(1);
+  enableOutput();
+  digitalWrite(MI, HIGH);
+  delay(1);
+  digitalWrite(MI, LOW);
+  delay(1);
+  disableOutput();
+}
+
+void putOut(byte data)
+{
+  PORTD = data;
+  delay(1);
+}
+
+void writeInstruction(byte instr)
+{
+  putOut(instr);
+  enableOutput();
+  delay(1);
+  digitalWrite(RI, HIGH);
+  delay(1);
+  digitalWrite(RI, LOW);
+  delay(1);
+  disableOutput();
+  
+}
+
+byte buffer[512];
+
+void loop() {
+  if (Serial.available()) {
+
+    int p_size = Serial.readBytes(buffer, sizeof(buffer));
+    Serial.write(0);
+
+    reset();
+    disable_cu();
+
+    delay(1);
+    enableClock();
+    delay(1);
+    for (int i = 0; i < p_size; i ++)
+    {
+      setAddress(i);
+      delay(1);
+      writeInstruction(buffer[i]);
+      delay(1);
+    }
+
+    disableClock();
+    digitalWrite(HL, LOW);
+    reset();
+    enable_cu();
+  }
+  disableOutput();
+
+}
